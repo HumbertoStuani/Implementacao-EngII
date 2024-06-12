2c276ff3d4237fd013df5d8774df3a00dbfe63b2
Samuel <samueldornelles1@gmail.com>
2024-06-12 20:13:19 -0300

    add busca CPF

---
+++ sapcbackend/src/main/java/sapc/sapcbackend/controllers/PessoaController.java
@@ -0,0 +1,27 @@
+package sapc.sapcbackend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sapc.sapcbackend.db.entities.Pessoa;
+import sapc.sapcbackend.db.repositories.PessoaRepositoryCustom;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pessoa")
+public class PessoaController {
+
+    @Autowired
+    private PessoaRepositoryCustom pessoaRepository;
+
+    @GetMapping("/buscarPorCpf")
+    public ResponseEntity<Long> getPessoaIdByCpf(@RequestParam String cpf) {
+        Optional<Pessoa> pessoa = pessoaRepository.findByCpf(cpf);
+        if (pessoa.isPresent()) {
+            return ResponseEntity.ok(pessoa.get().getId());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
